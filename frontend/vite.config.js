ce89a705c0261ed0b26ee45b29c24f2cee25a41c
Wing <eamonn.kearns@so-4pt.net>
2025-10-29 18:06:35 +0000

    Fixed the introduced bug that meant things weren't updating. ??0 saves the day

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@CharacterEditor': path.resolve(__dirname, 'src/Components/CharacterEditor'),
+            '@inc': path.resolve(__dirname, 'src/inc'),
+        },
+    },
 })
