ae6f76323ac328b16d5fa01dea4192ed440679ff
Wing <eamonn.kearns@so-4pt.net>
2025-12-14 15:55:55 +0000

    Changing the row

---
+++ backend/Discord/bot-commands/edit-web-sheet.js
@@ -0,0 +1,29 @@
+import userHash from "./inc/userHashFunction.js";
+import {SlashCommandBuilder, MessageFlags} from 'discord.js';
+import conf from '../../../conf.js';
+
+
+export default({controller})=>({
+    data: new SlashCommandBuilder()
+        .setName('edit-web-sheet')
+        .setDescription("Load a web sheet for editing, all characters can be found with /show-characters.")
+        .addStringOption(option =>
+            option
+                .setName('nanoid')
+                .setDescription("The unique id of the pc to fetch")
+                .setRequired(true))
+    ,
+    async execute(interaction) {
+        const hash = await userHash(interaction);
+        const nanoid = interaction.options.getString('nanoid');
+
+        controller.getSheetByHashAndNanoid({hash, nanoid}).then(()=>{
+            interaction.reply({content:`Your web sheet can be found at ${conf.frontend.url}/characters/${nanoid}/edit`, flags: MessageFlags.Ephemeral});
+        }).catch(_err=>{
+            console.log(_err);
+            interaction.reply({content:`Your have no sheet on this server with that id`, flags: MessageFlags.Ephemeral});
+        });
+
+        console.log("response sent");
+    },
+});
