66910d8d01a7ca53cb7154c69fd97d146dcf30bf
Wing <eamonn.kearns@so-4pt.net>
2025-12-23 17:07:19 +0000

    Printable sheets done(ish).

--- frontend/src/components/CharacterSheet/PersonalDetails.jsx
+++ frontend/src/components/CharacterSheet/PersonalDetails.jsx
@@ -7,25 +7,52 @@ function PersonalDetails({sheet})
     return <>
         <h2>Personal Details</h2>
         <Row id="personalDetails">
-        <Col md={2} lg={2}><strong>Name</strong></Col>
-        <Col md={2} lg={2}>{sheet.name}</Col>
-        <Col md={2} lg={2}><strong>Court</strong></Col>
-        <Col md={2} lg={2}>{sheet.court}</Col>
-        <Col md={2} lg={2}><strong>Seeming</strong></Col>
-        <Col md={2} lg={2}>{sheet.seeming}</Col>
-        <Col md={2} lg={2}><strong>Player</strong></Col>
-        <Col md={2} lg={2}>{sheet.player}</Col>
-        <Col md={2} lg={2}><strong>Legacies</strong></Col>
-        <Col md={2} lg={2}>{sheet.legacies.seelie} / {sheet.legacies.unseelie}</Col>
-        <Col md={2} lg={2}><strong>Kith</strong></Col>
-        <Col md={2} lg={2}>{sheet.kith}</Col>
-        <Col md={2} lg={2}><strong>Chronicle</strong></Col>
-        <Col md={2} lg={2}>{sheet.chronicle}</Col>
-        <Col md={2} lg={2}><strong>House</strong></Col>
-        <Col md={2} lg={2}>{sheet.house}</Col>
-        <Col md={2} lg={2}><strong>Motley</strong></Col>
-        <Col md={2} lg={2}>{sheet.motley}</Col>
-    </Row></>
+            <Col>
+                <Row>
+                    <Col><strong>Name</strong></Col>
+                    <Col>{sheet.name}</Col>
+                </Row>
+                <Row>
+                    <Col><strong>Player</strong></Col>
+                    <Col>{sheet.player}</Col>
+                </Row>
+                <Row>
+                    <Col><strong>Chronicle</strong></Col>
+                    <Col>{sheet.chronicle}</Col>
+                </Row>
+            </Col>
+            <Col>
+                <Row>
+                    <Col><strong>Court</strong></Col>
+                    <Col>{sheet.court}</Col>
+                </Row>
+                <Row>
+                    <Col><strong>Legacies</strong></Col>
+                    <Col>{sheet.legacies.seelie} / {sheet.legacies.unseelie}</Col>
+                </Row>
+                <Row>
+                    <Col><strong>House</strong></Col>
+                    <Col>{sheet.house}</Col>
+                </Row>
+            </Col>
+            <Col>
+                <Row>
+                    <Col><strong>Seeming</strong></Col>
+                    <Col>{sheet.seeming}</Col>
+                </Row>
+                <Row>
+                    <Col><strong>Kith</strong></Col>
+                    <Col>{sheet.kith}</Col>
+                </Row>
+                <Row>
+                    <Col><strong>Motley</strong></Col>
+                    <Col>{sheet.motley}</Col>
+                </Row>
+            </Col>
+
+        </Row>
+    </>
 }
+
 
 export default memo(PersonalDetails);
--- frontend/src/components/CharacterSheet/Traits/TraitDots.jsx
+++ frontend/src/components/CharacterSheet/Traits/TraitDots.jsx
@@ -1,7 +1,6 @@
 import {memo} from 'react';
 
 function TraitDots({ level = 0, maxLevel = 5 }) {
-    console.log(Math.max(maxLevel,level), level, maxLevel);
     return (
         <>
             {Array.from({ length: Math.max(maxLevel,level) }).map((_, i) => (
